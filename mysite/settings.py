04320795bc32d8579b5755e529473643bb928f3a
Kevin Cripe <kevincripe6@gmail.com>
2024-04-15 20:32:57 -0500

    typo

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*n@qnp0meo2$7v&@=peivnya+mkvfpsgff97m61qw$=kn4o^85
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['kevincripe.pythonanywhere.com',
+ALLOWED_HOSTS = ['kcripe15.pythonanywhere.com',
 '127.0.0.1']
 
 
